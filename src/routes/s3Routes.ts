ca5079dc44576ed4e5667b7190c3f85734e8e76c
ThiagoFPires <contato.thiagofreitasp@gmail.com>
2025-03-01 22:07:37 -0300

    Efetuado remoção de arquivos desnecessários.

--- src/routes/s3Routes.ts
+++ src/routes/s3Routes.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import { upload } from '../middlewares/uploadMiddleware';
 import { uploadFile, viewFile } from '../controllers/s3Controller';
-import { asyncHandler } from '../utils/asyncHandler';
 
 const router = express.Router();
 
-router.post('/upload', upload.single('file'), asyncHandler(uploadFile));
-router.get('/view/:filename', asyncHandler(viewFile));
+router.post('/upload', uploadFile);
+router.get('/view/:filename',viewFile);
 
 export default router;
