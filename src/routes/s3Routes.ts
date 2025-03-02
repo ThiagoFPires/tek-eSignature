961f1ec3924d2e4190625f6dd8add477adf350ea
ThiagoFPires <contato.thiagofreitasp@gmail.com>
2025-03-01 22:37:25 -0300

    Revert "Efetuado remoção de arquivos desnecessários."
    This reverts commit ca5079dc44576ed4e5667b7190c3f85734e8e76c.

--- src/routes/s3Routes.ts
+++ src/routes/s3Routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { upload } from '../middlewares/uploadMiddleware';
 import { uploadFile, viewFile } from '../controllers/s3Controller';
+import { asyncHandler } from '../utils/asyncHandler';
 
 const router = express.Router();
 
-router.post('/upload', uploadFile);
-router.get('/view/:filename',viewFile);
+router.post('/upload', upload.single('file'), asyncHandler(uploadFile));
+router.get('/view/:filename', asyncHandler(viewFile));
 
 export default router;
