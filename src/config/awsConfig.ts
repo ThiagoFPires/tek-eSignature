8cba2ea2770148848e6eba4204d0ee7ea748ef49
ThiagoFPires <contato.thiagofreitasp@gmail.com>
2025-03-04 14:16:44 -0300

    removido logs utilizados para teste

--- src/config/awsConfig.ts
+++ src/config/awsConfig.ts
@@ -5,12 +5,6 @@ import dotenv from 'dotenv';
 // Carregar as variáveis de ambiente
 dotenv.config();
 
-// Verificar se as variáveis de ambiente estão sendo lidas corretamente
-console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
-console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
-console.log('AWS_REGION:', process.env.AWS_REGION);
-console.log('AWS_BUCKET_NAME:', process.env.AWS_BUCKET_NAME);
-
 // Configuração do cliente S3
 export const s3 = new S3({
   region: process.env.AWS_REGION || 'us-east-1', // Região do S3, com fallback se não estiver no .env
